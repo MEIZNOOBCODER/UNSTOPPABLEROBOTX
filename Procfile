3c7dc8fd60dc1d72d5df06a6a5d89e17ac94e165
Shaurya Singh <gaurav1152003@gmail.com>
2022-04-22 17:42:58 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python3 -m UnstoppableRobot 
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE Unstoppable Robot
+:: Enables virtual env mode and then starts Unstoppable
+env\scripts\activate.bat && py -m UnstoppableRobot
