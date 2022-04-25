8e7cb2128e6b671abf7ea37f7d3bd3a3f5cd8bce
Shaurya Singh <gaurav1152003@gmail.com>
2022-04-25 22:17:37 +0530

    Update events.py

--- UnstoppableRobot/events.py
+++ UnstoppableRobot/events.py
@@ -199,7 +199,7 @@ def load_module(shortname):
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         mod.register = register
-        mod.Unstoppablerobot = Unstoppablerobot
+        mod.UnstoppableRobot = UnstoppableRobot
         mod.tbot = telethn
         mod.logger = logging.getLogger(shortname)
         spec.loader.exec_module(mod)
