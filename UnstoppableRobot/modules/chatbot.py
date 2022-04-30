7ca0e734d88f2ef6fa4fab2758fac9edd399c781
Shaurya Singh <gaurav1152003@gmail.com>
2022-04-30 23:53:10 +0530

    Update chatbot.py

--- UnstoppableRobot/modules/chatbot.py
+++ UnstoppableRobot/modules/chatbot.py
@@ -3,7 +3,7 @@ import re
 import os
 import html
 import requests
-import FallenRobot.modules.sql.chatbot_sql as sql
+import UnstoppableRobot.modules.sql.chatbot_sql as sql
 
 from time import sleep
 from telegram import ParseMode
