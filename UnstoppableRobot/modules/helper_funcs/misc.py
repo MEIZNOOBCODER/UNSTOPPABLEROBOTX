bd9ffeae64092fa234e9b233a29fb9273d73f730
™NixonHYPER® <92142624+HYPER-AD17@users.noreply.github.com>
2022-05-02 07:05:47 +0530

    Update misc.py

--- UnstoppableRobot/modules/helper_funcs/misc.py
+++ UnstoppableRobot/modules/helper_funcs/misc.py
@@ -90,7 +90,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
                 EqInlineKeyboardButton(
                     "◁", callback_data="{}_prev({})".format(prefix, modulo_page)
                 ),
-                EqInlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="fallen_back"),
+                EqInlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="Unstoppable_back"),
                 EqInlineKeyboardButton(
                     "▷", callback_data="{}_next({})".format(prefix, modulo_page)
                 ),
@@ -98,7 +98,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         ]
 
     else:
-        pairs += [[EqInlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="fallen_back")]]
+        pairs += [[EqInlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="Unstoppable_back")]]
 
     return pairs
 
