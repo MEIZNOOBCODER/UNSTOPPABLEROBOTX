734cc364e3d181333e61adc77ebb81759fd25399
™NixonHYPER® <92142624+HYPER-AD17@users.noreply.github.com>
2022-05-12 01:13:11 +0530

    Update alive.py

--- UnstoppableRobot/modules/alive.py
+++ UnstoppableRobot/modules/alive.py
@@ -10,23 +10,25 @@ from UnstoppableRobot.events import register
 from UnstoppableRobot import telethn as tbot
 
 
-PHOTO = [
-    "https://te.legra.ph/file/a023c5a9071e18e406c3b.jpg",
-    "https://telegra.ph/file/7bd111132fce009e4605e.jpg",
-    "https://telegra.ph/file/804a5f9a3c32bac1ae15c.jpg",
-    "https://telegra.ph/file/43edaa8914b7ce8998336.jpg",
-    "https://telegra.ph/file/abed92d9b3ff409793324.jpg",
+ALIVE_STUFF = [
+    "https://telegra.ph/file/9c91d02c0b3ef63641963.mp4",
+    "https://telegra.ph/file/a10878077476b20abdf23.mp4",
+    "https://telegra.ph/file/d35409b383589b0199776.mp4",
+    "https://telegra.ph/file/d1d8dbe8730178f258cf1.mp4",
+    "https://telegra.ph/file/98e91150ca5942615f695.mp4",
+    "https://telegra.ph/file/1e72c3b2532dad19332c3.mp4",
+    "https://telegra.ph/file/415b0b67cf946ab5f3ec4.mp4",
 ]
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [•ᴜɴsᴛ🅞 ᴘᴘᴀʙʟᴇ•] ʟɪsᴀ​**\n━━━━━━━━━━━━━━━━━━━\n\n"
+  TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\nɪ ᴀᴍ [•ᴜɴsᴛ🅞 ᴘᴘᴀʙʟᴇ•] ʟɪsᴀ​**\n\n━━━━━━━━━━━━━━━━━━━\n\n"
   TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [•ᴜɴsᴛ🅞 ᴘᴘᴀʙʟᴇ•](https://t.me/shaurya_here)** \n\n"
   TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
   TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
-  TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
+  TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n\n━━━━━━━━━━━━━━━━━\n\n"
   BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/misslisa_robot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/S_UNSTOPPABLE")]]
-  ran = random.choice(PHOTO)
-  await tbot.send_file(event.chat_id, ran, caption=TEXT,  buttons=BUTTON)
+  anon_randi = random.choice(ALIVE_STUFF)
+  await tbot.send_file(event.chat_id, anon_randi, caption=TEXT,  buttons=BUTTON)
 
 ## Alive mod
